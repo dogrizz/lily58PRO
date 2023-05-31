560338f18f682754ffe3cdbe6749218a111ddc3e
Dominik Zmitrowicz <dogrizz@gmail.com>
2023-05-31 11:34:00 +0200

    Updated for qmk 0.21

--- config.h
+++ config.h
@@ -30,7 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 350
-#define IGNORE_MOD_TAP_INTERRUPT
 // Underglow
 /*
 #undef RGBLED_NUM
