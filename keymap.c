ec8a28cc4b1db573338202b3bc0accf53feb3188
Dominik Zmitrowicz <dogrizz@gmail.com>
2023-08-16 01:29:22 +0200

    expand on the corneish layout and go back to regular layer buttons

--- keymap.c
+++ keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |    |      |      |      |      |       |                    |      |      |      |      |      |       |
+ * |    |   1   |   2   |  3   |  4   |   5  |                    |  6   |  7   |  8   |  9   |  0   |       |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  <-  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -41,17 +41,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  `   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      | LCtrl | LAlt| /Space/Low/   \Enter/Rais \  | LGUI | Ralt |      |
+ *                   |LCtrl | LAlt | Lower  / Space /       \Enter \  | Raise| LGUI | Ralt |      
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `-------------------''-------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT(
-  XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX,   KC_1,  KC_2,  KC_3,    KC_4,     KC_5,                      KC_6,   KC_7,     KC_8,   KC_9,     KC_0,  XXXXXXX,
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,  XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_TILD,
-                      XXXXXXX, KC_LCTL, KC_LALT, LT(_LOWER,KC_SPC), LT(_RAISE,KC_ENT), KC_LGUI, KC_RALT, XXXXXXX
+                       KC_LCTL, KC_LALT, _LOWER,     KC_SPC,    KC_ENT  , _RAISE, KC_LGUI, KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
   _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4 ,  KC_F5,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DELETE, _______,
+  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4 ,  KC_F5,                     LT(KC_HOME,KC_LEFT), LT(KC_PGDN, KC_DOWN), LT(KC_PGUP, KC_UP), LT(KC_END, KC_RGHT), KC_DELETE, _______,
   _______, KC_F6  , KC_F7  , KC_F8  , KC_F9 ,  KC_F10,  _______, _______, KC_F11 , KC_F12,  _______, _______, _______, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
