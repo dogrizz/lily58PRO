d2377c0de406a245c98991fc7d58ae488bc75bdf
Dominik Zmitrowicz <dogrizz@gmail.com>
2023-08-16 18:21:14 +0200

    Try to imitate zmk hold-taps

--- config.h
+++ config.h
@@ -25,7 +25,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 
 #define QUICK_TAP_TERM 0
-#define TAPPING_TERM 350
+#define TAPPING_TERM 300
 #define NO_AUTO_SHIFT_ALPHA
 #define NO_AUTO_SHIFT_SPECIALS
-#define AUTO_SHIFT_TIMEOUT 350
+#define AUTO_SHIFT_TIMEOUT 300
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
--- keymap.c
+++ keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  `   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LCtrl | LAlt | Lower  / Space /       \Enter \  | Raise| LGUI | Ralt |      
+ *                   |LCtrl | LAlt | Lower  / Space /       \Enter \  | Raise|  | LGUI |      
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `-------------------''-------'           '------''--------------------'
  */
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX,   KC_1,  KC_2,  KC_3,    KC_4,     KC_5,                      KC_6,   KC_7,     KC_8,   KC_9,     KC_0,  XXXXXXX,
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,  XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_TILD,
-                       KC_LCTL, KC_LALT, _LOWER,     KC_SPC,    KC_ENT  , _RAISE, KC_LGUI, KC_RALT
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,  XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_GRAVE,
+                       KC_LCTL, KC_LALT, LOWER,     KC_SPC,    KC_ENT  , RAISE, KC_RALT, KC_LGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -70,8 +70,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                                 _______, _______, _______,_______, _______, _______,
   _______, KC_1   , KC_2,    KC_3,    KC_4,    KC_5   ,                                 KC_6,   KC_7,     KC_8,   KC_9,     KC_0,    _______,
-  _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT,KC_MS_WH_UP,                   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
-  _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2, KC_MS_WH_DOWN, _______,_______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_GRAVE,
+  _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT,KC_MS_WH_UP,                   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRAVE,
+  _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2, KC_MS_WH_DOWN, _______,_______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* RAISE
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
   _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4 ,  KC_F5,                     LT(KC_HOME,KC_LEFT), LT(KC_PGDN, KC_DOWN), LT(KC_PGUP, KC_UP), LT(KC_END, KC_RGHT), KC_DELETE, _______,
+  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4 ,  KC_F5,                       LT(0,KC_LEFT), LT(0,KC_DOWN), LT(0,KC_UP), LT(0,KC_RGHT), KC_DELETE, _______,
   _______, KC_F6  , KC_F7  , KC_F8  , KC_F9 ,  KC_F10,  _______, _______, KC_F11 , KC_F12,  _______, _______, _______, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
@@ -224,7 +224,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef OLED_ENABLE
     set_keylog(keycode, record);
 #endif
-    // set_timelog();
+  }
+  switch (keycode) {
+    case LT(0,KC_UP):
+        if (!record->tap.count && record->event.pressed) {
+            tap_code16(KC_PGUP);
+            return false;
+        }
+        return true;            
+    case LT(0,KC_DOWN):
+        if (!record->tap.count && record->event.pressed) {
+            tap_code16(KC_PGDN);
+            return false;
+        }
+        return true;            
+    case LT(0,KC_LEFT):
+        if (!record->tap.count && record->event.pressed) {
+            tap_code16(KC_HOME);
+            return false;
+        }
+        return true;
+    case LT(0,KC_RIGHT):
+        if (!record->tap.count && record->event.pressed) {
+            tap_code16(KC_END);
+            return false;
+        }
+          return true;
   }
   return true;
 }
